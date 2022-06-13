9e52b4a4010131bc27b428feabeb8084bc531e83
Nate H <69021466+nrhancock@users.noreply.github.com>
2022-06-13 12:25:30 -0400

    simpleCount
    Really basic, Showing how to use a simple math loop for counting and changing values in a range

---
+++ simpleCount.py
@@ -0,0 +1,12 @@
+for n in range(1, 1001):
+	if n % 7 == 0 and n % 9 == 0:
+		print ('Hello Goodbye')
+	
+	elif n % 7 == 0:
+		print ('Hello')
+	
+	elif n % 9 == 0:
+		print ('Goodbye')
+	
+	else:
+		print(n)
